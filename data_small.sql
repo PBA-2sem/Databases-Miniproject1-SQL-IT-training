904c031ecbda2b040b29ba226834dcd86acef5fa
Stanislav Novitski <Stanislav.Novitski@ibm.com>
2020-02-24 12:14:53 +0100

    more test data

--- data_small.sql
+++ data_small.sql
@@ -31,6 +31,306 @@ INSERT INTO trainee (name, email)
 INSERT INTO trainee (name, email)
     VALUES ('Alexander', 'aa@email.com');
 
+INSERT INTO trainee (name, email)
+    VALUES ('Zea Rubica', 'zrubica0@si.edu');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Giorgi Lorraway', 'glorraway1@123-reg.co.uk');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ermin Harriday', 'eharriday2@virginia.edu');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Wynny McKirdy', 'wmckirdy3@reference.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Isa Matteacci', 'imatteacci4@census.gov');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Justinn Dimitriev', 'jdimitriev5@newyorker.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ruperto Phayre', 'rphayre6@telegraph.co.uk');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Sawyer Dorro', 'sdorro7@dion.ne.jp');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Valentijn McCudden', 'vmccudden8@ask.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Kelsy Goode', 'kgoode9@gov.uk');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Killy Denney', 'kdenneya@ameblo.jp');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ingrid Bourthouloume', 'ibourthouloumeb@cargocollective.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Karon Kebell', 'kkebellc@google.com.au');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Anna Tollerfield', 'atollerfieldd@economist.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Lanie Matveichev', 'lmatveicheve@cocolog-nifty.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Robenia Wilflinger', 'rwilflingerf@github.io');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Shermie Satterley', 'ssatterleyg@desdev.cn');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Alexis Dumbrill', 'adumbrillh@yahoo.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Cully Rodbourne', 'crodbournei@360.cn');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ally De Brett', 'adej@imgur.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Field Kiff', 'fkiffk@answers.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Harriett Paty', 'hpatyl@ycombinator.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Valencia Lyddy', 'vlyddym@tinypic.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Lavinie Pimlott', 'lpimlottn@ted.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Godwin Dahmke', 'gdahmkeo@sogou.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Zolly Wainer', 'zwainerp@fotki.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Anabel Trittam', 'atrittamq@tripadvisor.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Tandi Dennitts', 'tdennittsr@baidu.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Darryl Webland', 'dweblands@blogtalkradio.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Calypso O''Tierney', 'cotierneyt@furl.net');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Moise Knowles', 'mknowlesu@symantec.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Isador Shillabeare', 'ishillabearev@webs.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Arney Veck', 'aveckw@artisteer.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Calli McNally', 'cmcnallyx@homestead.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Gerianne Hauch', 'ghauchy@webs.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Frieda Trattles', 'ftrattlesz@nifty.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Leann Mc Caghan', 'lmc10@earthlink.net');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Emilia Mahmood', 'emahmood11@squarespace.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Waldon Dripps', 'wdripps12@lulu.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Imojean Retchless', 'iretchless13@abc.net.au');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Pinchas Tellenbach', 'ptellenbach14@hubpages.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Lay Brightie', 'lbrightie15@exblog.jp');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ernaline Sally', 'esally16@blogspot.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Wye Smethurst', 'wsmethurst17@networkadvertising.org');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Eliot Izzat', 'eizzat18@nymag.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Estelle Ord', 'eord19@squidoo.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Dalenna Terlinden', 'dterlinden1a@thetimes.co.uk');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Tanya Du Plantier', 'tdu1b@nytimes.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Karna Bushaway', 'kbushaway1c@wufoo.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Gil Melanaphy', 'gmelanaphy1d@businessinsider.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Lindsey Jump', 'ljump1e@moonfruit.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Pincas Wallbank', 'pwallbank1f@ucoz.ru');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Minny Cribbin', 'mcribbin1g@sphinn.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ralph Jakoviljevic', 'rjakoviljevic1h@ovh.net');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Kimbra Blazey', 'kblazey1i@spiegel.de');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Queenie Lowther', 'qlowther1j@europa.eu');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Thom Cadogan', 'tcadogan1k@gov.uk');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Philippa Clifft', 'pclifft1l@cafepress.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Terrence Dullaghan', 'tdullaghan1m@google.pl');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Buffy Lorman', 'blorman1n@devhub.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Lanette Raw', 'lraw1o@google.pl');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Eli Dreng', 'edreng1p@jimdo.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Anita Fidelus', 'afidelus1q@people.com.cn');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Babette Sterke', 'bsterke1r@ucla.edu');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Sandi Higgen', 'shiggen1s@jugem.jp');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Blair Sirmon', 'bsirmon1t@istockphoto.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Nonie Desorts', 'ndesorts1u@reuters.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Noelyn Van Der Weedenburg', 'nvan1v@alexa.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Tish Harold', 'tharold1w@dmoz.org');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Almeda Barbery', 'abarbery1x@macromedia.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Angelina Chipman', 'achipman1y@cnbc.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Franklyn Swatland', 'fswatland1z@tinyurl.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Bryn Karlolak', 'bkarlolak20@soup.io');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Rory Hassard', 'rhassard21@senate.gov');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Bambi Alldread', 'balldread22@sakura.ne.jp');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Alfy Girkins', 'agirkins23@abc.net.au');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Netty Westbury', 'nwestbury24@nifty.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Kala Springer', 'kspringer25@slate.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Dame Kerwin', 'dkerwin26@wiley.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Nikolia Landes', 'nlandes27@independent.co.uk');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Camala Happel', 'chappel28@furl.net');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Gunilla Walley', 'gwalley29@theguardian.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Karina Milthorpe', 'kmilthorpe2a@flavors.me');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Fayina Pendreigh', 'fpendreigh2b@addthis.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Albert Wims', 'awims2c@facebook.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Cordelia Boice', 'cboice2d@com.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ellynn Garlinge', 'egarlinge2e@baidu.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Chantalle Tapp', 'ctapp2f@skype.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Gearard Shimmans', 'gshimmans2g@noaa.gov');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Peggy Odgers', 'podgers2h@themeforest.net');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Dorella Brandreth', 'dbrandreth2i@ocn.ne.jp');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ruby Coppenhall', 'rcoppenhall2j@typepad.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Trevar Bone', 'tbone2k@cloudflare.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Roseanne Thornhill', 'rthornhill2l@ocn.ne.jp');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Allie Shepherdson', 'ashepherdson2m@tinyurl.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Jeffrey Arghent', 'jarghent2n@nsw.gov.au');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Kristen Patinkin', 'kpatinkin2o@flavors.me');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Tomasine Symes', 'tsymes2p@epa.gov');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Ulrica Stainer', 'ustainer2q@oakley.com');
+
+INSERT INTO trainee (name, email)
+    VALUES ('Lorrayne Scullard', 'lscullard2r@google.com');
+
 -- Create Instructor, will get id 1
 INSERT INTO instructor (name)
     VALUES ('HARRY');
@@ -38,7 +338,91 @@ INSERT INTO instructor (name)
 INSERT INTO instructor (name)
     VALUES ('HERMIONE');
 
--- Create 3 teaching teams with id 1,2,3
+INSERT INTO instructor (name)
+    VALUES ('Elianora Cecchetelli');
+
+INSERT INTO instructor (name)
+    VALUES ('Bibi Grishakin');
+
+INSERT INTO instructor (name)
+    VALUES ('Bobbi Montague');
+
+INSERT INTO instructor (name)
+    VALUES ('Tab Lofting');
+
+INSERT INTO instructor (name)
+    VALUES ('Parry Patrickson');
+
+INSERT INTO instructor (name)
+    VALUES ('Katrina Denisovo');
+
+INSERT INTO instructor (name)
+    VALUES ('Ryan Graveson');
+
+INSERT INTO instructor (name)
+    VALUES ('Veda Middlebrook');
+
+INSERT INTO instructor (name)
+    VALUES ('Riva Hailes');
+
+INSERT INTO instructor (name)
+    VALUES ('Linzy Dayment');
+
+INSERT INTO instructor (name)
+    VALUES ('Kaspar Hearley');
+
+INSERT INTO instructor (name)
+    VALUES ('Shanna Hyland');
+
+INSERT INTO instructor (name)
+    VALUES ('Nicky Whitworth');
+
+INSERT INTO instructor (name)
+    VALUES ('Sadella Sivewright');
+
+INSERT INTO instructor (name)
+    VALUES ('Gaston Cicullo');
+
+INSERT INTO instructor (name)
+    VALUES ('Lavinie McGeady');
+
+INSERT INTO instructor (name)
+    VALUES ('Quinn Cicchillo');
+
+INSERT INTO instructor (name)
+    VALUES ('Micky Crosier');
+
+INSERT INTO instructor (name)
+    VALUES ('Ulysses Gordge');
+
+INSERT INTO instructor (name)
+    VALUES ('Daloris Stowte');
+
+INSERT INTO instructor (name)
+    VALUES ('Garey Bisco');
+
+INSERT INTO instructor (name)
+    VALUES ('Hubert Ponting');
+
+INSERT INTO instructor (name)
+    VALUES ('Margareta Ottee');
+
+INSERT INTO instructor (name)
+    VALUES ('Lane Haggarth');
+
+INSERT INTO instructor (name)
+    VALUES ('Halley Mayes');
+
+INSERT INTO instructor (name)
+    VALUES ('Phaedra Guillet');
+
+INSERT INTO instructor (name)
+    VALUES ('Kristoffer Winwright');
+
+INSERT INTO instructor (name)
+    VALUES ('Hesther Reckless');
+
+-- Create  teaching teams with id 1,2,3
 INSERT INTO teaching_team (teaching_team_id)
     VALUES (1);
 
@@ -47,6 +431,42 @@ INSERT INTO teaching_team (teaching_team_id)
 
 INSERT INTO teaching_team (teaching_team_id)
     VALUES (3);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (4);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (5);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (6);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (7);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (8);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (9);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (10);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (11);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (12);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (13);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (14);
+
+INSERT INTO teaching_team (teaching_team_id)
+    VALUES (15);
 
 -- Assign instructor with id 1 to teaching team 1 and 2
 INSERT INTO teachingteam_instructor (teaching_team_id, instructor_id)
@@ -58,8 +478,189 @@ INSERT INTO teachingteam_instructor (teaching_team_id, instructor_id)
 INSERT INTO teachingteam_instructor (teaching_team_id, instructor_id)
     VALUES (1, 2);
 
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (1, 1);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (2, 2);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (3, 3);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (4, 4);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (5, 5);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (6, 6);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (7, 7);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (8, 8);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (9, 9);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (10, 10);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (11, 11);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (12, 12);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (13, 13);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (14, 14);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (15, 15);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (1, 16);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (2, 17);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (3, 18);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (4, 19);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (5, 20);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (6, 21);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (7, 22);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (8, 23);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (9, 24);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (10, 25);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (11, 26);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (12, 27);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (13, 28);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (14, 29);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (15, 30);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (1, 30);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (2, 29);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (3, 28);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (4, 27);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (5, 26);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (6, 25);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (7, 24);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (8, 23);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (9, 22);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (10, 21);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (11, 20);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (12, 19);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (13, 18);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (14, 17);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (15, 16);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (1, 15);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (2, 14);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (3, 13);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (4, 12);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (5, 11);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (6, 10);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (7, 9);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (8, 8);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (9, 7);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (10, 6);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (11, 5);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (12, 4);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (13, 3);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (14, 2);
+
+INSERT INTO teaching_team (teaching_team_id, instructor_id)
+    VALUES (15, 1);
+
 -- Enrollment TODO
 -- Asssign teaching team to course
 
 INSERT INTO teaches (teaching_team_id, course_id)
     VALUES (1, 1);
+

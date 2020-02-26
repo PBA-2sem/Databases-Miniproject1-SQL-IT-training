906fe5b10a03b2e0c08818919ad68b4855ef3ac9
DDomino <Andreasj54@gmail.com>
2020-02-26 21:02:54 +0100

    gaar i if

--- enrollmentredo.sql
+++ enrollmentredo.sql
@@ -1,25 +1,26 @@
 CREATE OR REPLACE FUNCTION check_max_courses_for_trainee ()
      RETURNS TRIGGER AS $$
  DECLARE
-    presence text;
-    season text;
-    year text;
+    --presence text;
+  --  season text;
+--    year text;
  BEGIN
- presence = 'SELECT presence FROM course WHERE course_id =' || NEW.course_id;
- season = 'SELECT season FROM course WHERE course_id =' || NEW.course_id;
- year = 'SELECT year FROM course WHERE course_id =' || NEW.course_id;
+-- presence = 'SELECT presence FROM course WHERE course_id =' || NEW.course_id;
+-- season = 'SELECT season FROM course WHERE course_id =' || NEW.course_id;
+--year = 'SELECT year FROM course WHERE course_id =' || NEW.course_id;
 
  IF(SELECT 1 FROM enrollment JOIN course on (enrollment.course_id = course.course_id)
 	WHERE enrollment.trainee_id = NEW.trainee_id 
-	AND graduated = 'false' 
+	AND graduated = false 
 	AND course.year = (SELECT course.year FROM course WHERE course_id = NEW.course_id)
 	AND course.season = (SELECT course.season FROM course WHERE course_id = NEW.course_id)
 	HAVING COUNT(*) >=1)
  THEN
- 	IF(presence = 'false')
- 	THEN
+ 	IF((SELECT presence FROM course WHERE course_id = NEW.course_id) = 'false')
+	THEN
+	RAISE NOTICE 'in false';
  		IF(SELECT 1 FROM enrollment WHERE trainee_id = NEW.trainee_id 
-		AND graduated = 'false' 
+		AND graduated = false 
 		AND course.year = (SELECT course.year FROM course WHERE course_id = NEW.course_id)
 		AND course.season = (SELECT course.season FROM course WHERE course_id = NEW.course_id)
 		HAVING COUNT(*) >= 3)
@@ -27,10 +28,11 @@ CREATE OR REPLACE FUNCTION check_max_courses_for_trainee ()
  		RAISE EXCEPTION 'Trainee is assigned to too many courses';
 		END IF;
 	END IF;
-	IF(presence = 'true')
+	IF((SELECT presence FROM course WHERE course_id = NEW.course_id) = 'true')
 		THEN
+			RAISE NOTICE 'in true';
 			IF(SELECT 1 FROM enrollment WHERE trainee_id = NEW.trainee_id
-			AND graduated = 'false' 
+			AND graduated = false 
 			AND course.year = (SELECT course.year FROM course WHERE course_id = NEW.course_id)
 			AND course.season = (SELECT course.season FROM course WHERE course_id = NEW.course_id)
 		    HAVING COUNT(*) >= 1)

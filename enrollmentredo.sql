cb03f09c3fe53267681e553f46eb1e49764c37a9
DDomino <Andreasj54@gmail.com>
2020-02-26 21:47:18 +0100

    ...1

--- enrollmentredo.sql
+++ enrollmentredo.sql
@@ -1,21 +1,7 @@
 CREATE OR REPLACE FUNCTION check_max_courses_for_trainee ()
      RETURNS TRIGGER AS $$
  DECLARE
-    --presence text;
-  --  season text;
---    year text;
  BEGIN
--- presence = 'SELECT presence FROM course WHERE course_id =' || NEW.course_id;
--- season = 'SELECT season FROM course WHERE course_id =' || NEW.course_id;
---year = 'SELECT year FROM course WHERE course_id =' || NEW.course_id;
-
- IF(SELECT 1 FROM enrollment JOIN course on (enrollment.course_id = course.course_id)
-	WHERE enrollment.trainee_id = NEW.trainee_id 
-	AND graduated = false 
-	AND course.year = (SELECT course.year FROM course WHERE course_id = NEW.course_id)
-	AND course.season = (SELECT course.season FROM course WHERE course_id = NEW.course_id)
-	HAVING COUNT(*) >=1)
- THEN
  	IF((SELECT presence FROM course  WHERE course_id = NEW.course_id) = 'false')
 	THEN
 	RAISE NOTICE 'in false';
@@ -43,7 +29,6 @@ CREATE OR REPLACE FUNCTION check_max_courses_for_trainee ()
 			RAISE EXCEPTION 'Trainee is assigned to too many courses';
 		END IF;
 	END IF;
-END IF;
  	RAISE NOTICE 'hej';
  	RETURN NEW;
  END;

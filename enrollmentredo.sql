3332c52e22117c5977b6af612e27350ff178d747
DDomino <Andreasj54@gmail.com>
2020-02-26 21:33:23 +0100

    ...

--- enrollmentredo.sql
+++ enrollmentredo.sql
@@ -22,19 +22,20 @@ CREATE OR REPLACE FUNCTION check_max_courses_for_trainee ()
  		IF(SELECT 1 FROM enrollment JOIN course ON (enrollment.course_id = course.course_id)
 		WHERE trainee_id = NEW.trainee_id 
 		AND graduated = false 
+		AND presence = (SELECT presence FROM course WHERE course_id = NEW.course_id)
 		AND course.year = (SELECT course.year FROM course WHERE course_id = NEW.course_id)
 		AND course.season = (SELECT course.season FROM course WHERE course_id = NEW.course_id)
 		HAVING COUNT(*) >= 3)
 		THEN
  		RAISE EXCEPTION 'Trainee is assigned to too many courses';
 		END IF;
-	END IF;
-	IF((SELECT presence FROM course WHERE course_id = NEW.course_id) = 'true')
+	ELSIF((SELECT presence FROM course WHERE course_id = NEW.course_id) = 'true')
 		THEN
-			RAISE NOTICE 'in true';
+		RAISE NOTICE 'in true';
 			IF(SELECT 1 FROM enrollment JOIN course on (enrollment.course_id = course.course_id)
 			WHERE trainee_id = NEW.trainee_id
-			AND graduated = false 
+			AND graduated = false
+			AND presence = (SELECT presence FROM course WHERE course_id = NEW.course_id)
 			AND course.year = (SELECT course.year FROM course WHERE course_id = NEW.course_id)
 			AND course.season = (SELECT course.season FROM course WHERE course_id = NEW.course_id)
 		    HAVING COUNT(*) >= 1)
